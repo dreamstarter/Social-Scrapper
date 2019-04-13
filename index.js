068cc73d020cd4eae3580ed330944c950c346f3f
Bryan Anderson <dreamstarter@gmail.com>
2019-04-12 21:07:38 -0400

    return both instagram and twitter followers

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import { getHTML, getTwitterFollowers } from './lib/scrapper';
+import { 
+    getHTML, 
+    getTwitterFollowers, 
+    getInstagramFollowers, 
+} from './lib/scrapper';
 
 async function go() {
-    const html = await getHTML('https://www.twitter.com/dreamstarter697');
-    const twCount = await getTwitterFollowers(html);
-    console.log(`You have ${twCount} Twitter followers`);
+    const iPromise = getHTML('https://www.instagram.com/dreamstarter');
+    const tPromise = getHTML('https://www.twitter.com/dreamstarter697');
+
+    const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
+
+    const igCount = await getInstagramFollowers(instagramHTML);
+    const twCount = await getTwitterFollowers(twitterHTML);
+
+    console.log(`You have ${twCount} Twitter followers and ${igCount} Instagram followers`);
 }
 
 go();
