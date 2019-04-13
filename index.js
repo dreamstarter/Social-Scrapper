9ced327fdabfb971b1dd4f63f35dc5663d264043
Bryan Anderson <dreamstarter@gmail.com>
2019-04-12 20:13:50 -0400

    initial functionality to return number of twitter followers

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import { getHTML } from './lib/scrapper';
+import { getHTML, getTwitterFollowers } from './lib/scrapper';
 
-console.log(getHTML());
+async function go() {
+    const html = await getHTML('https://www.twitter.com/dreamstarter697');
+    const twCount = await getTwitterFollowers(html);
+    console.log(`You have ${twCount} Twitter followers`);
+}
+
+go();
