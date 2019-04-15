007a1c534a4e66f30d1185a506f2fe6986d31431
Bryan Anderson <dreamstarter@gmail.com>
2019-04-15 13:10:35 -0400

    setup the express server

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-import { 
-    getHTML, 
-    getInstagramCount, 
-    getTwitterCount, 
-} from './lib/scrapper';
+import express from 'express';
+import { getInstagramCount, getTwitterCount} from './lib/scrapper';
 
-async function go() {
+const app = express();
+
+app.get('/scrape', async (request, response, next) => {
+    console.log('Scraping!');
     const [iCount, tCount] = await Promise.all([
         getInstagramCount(),
         getTwitterCount(),    
     ]);
     console.log(iCount, tCount);
-}
+    response.json({iCount, tCount})
+});
 
-go();
+app.listen(3000, () => {
+    console.log('Social Scrapper App is running on port 3000');
+});
