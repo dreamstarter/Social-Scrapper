0022bb3d8f6a4d4288777dfde107d0ad31d4ed7e
Bryan Anderson <dreamstarter@gmail.com>
2019-04-15 12:50:38 -0400

    added express and body-parser dependicies. refactored the twitter and instagram count functions.

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import { 
     getHTML, 
-    getTwitterFollowers, 
-    getInstagramFollowers, 
+    getInstagramCount, 
+    getTwitterCount, 
 } from './lib/scrapper';
 
 async function go() {
-    const iPromise = getHTML('https://www.instagram.com/dreamstarter');
-    const tPromise = getHTML('https://www.twitter.com/dreamstarter697');
-
-    const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
-
-    const igCount = await getInstagramFollowers(instagramHTML);
-    const twCount = await getTwitterFollowers(twitterHTML);
-
-    console.log(`You have ${twCount} Twitter followers and ${igCount} Instagram followers`);
+    const [iCount, tCount] = await Promise.all([
+        getInstagramCount(),
+        getTwitterCount(),    
+    ]);
+    console.log(iCount, tCount);
 }
 
 go();
